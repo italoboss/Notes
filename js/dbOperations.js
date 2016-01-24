75f14252855f841f23cc9eddef8bc6ff4a377d00
ItaloBoss <italoboss@gmail.com>
2016-01-24 16:58:31 -0300

    Autenticação
    Autenticação do usuário feita, mas as notas visualizadas ainda não estão
    sendo filtradas e as novas notas também não estão sendo associadas ao
    usuário.

--- js/dbOperations.js
+++ js/dbOperations.js
@@ -15,6 +15,37 @@ module.exports = {
             res.end();  
         });
     },
+    getUser: function(req, res) {    
+        var pg = require('pg');
+        var conString = process.env.DATABASE_URL || "pg://postgres:1234@localhost:5432/notesDB";
+        var client = new pg.Client(conString);
+        client.connect();
+        var query = client.query("SELECT * FROM users WHERE login = '" + req.query.login + "'");
+        query.on("row", function (row, result) { 
+            result.addRow(row);
+        });
+        query.on("end", function (result) {          
+            client.end();
+            if (result.rows.length <= 0) {
+                req.session.user = null;
+                res.write("Usuário nao existe!");
+                res.end();
+            }
+            else {
+                var aux = result.rows[0];
+                if (aux.password == req.query.password) {
+                    req.session.user = aux;
+                    res.write('Sucesso');
+                    res.end();
+                }
+                else {
+                    req.session.user = null;
+                    res.write("Senha incorreta!");
+                    res.end();
+                }
+            }
+        });
+    },
     getNotes: function(req, res) {
         var pg = require('pg');
         var conString = process.env.DATABASE_URL || "pg://postgres:1234@localhost:5432/notesDB";
